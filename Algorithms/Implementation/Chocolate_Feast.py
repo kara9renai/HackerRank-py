5b52c773b8b377751453a65fed8d4d5244b85ad0
ryota <r.enumura@icloud.com>
2021-09-25 23:40:44 +0900

    add new solutions

---
+++ Algorithms/Implementation/Chocolate_Feast.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+#
+# Complete the 'chocolateFeast' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER c
+#  3. INTEGER m
+#
+
+def chocolateFeast(n, c, m):
+    w = n // c
+    count = w
+    while w >= m:
+        w -= m
+        w += 1
+        count += 1
+    return count
+
+if __name__ == '__main__':
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        c = int(first_multiple_input[1])
+
+        m = int(first_multiple_input[2])
+
+        result = chocolateFeast(n, c, m)
+        
+        print(result)
