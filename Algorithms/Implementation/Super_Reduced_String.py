60d37f2ee65e2c7785f758575a78e7f09b8c2c68
ryota <r.enumura@icloud.com>
2021-10-19 01:33:17 +0900

    update Super_Reduced_String.py

--- Algorithms/Implementation/Super_Reduced_String.py
+++ Algorithms/Implementation/Super_Reduced_String.py
@@ -6,16 +6,21 @@
 # The function is expected to return a STRING.
 # The function accepts STRING s as parameter.
 #
+def create(s):
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            s = s[:i] + s[i+2:]
+            return s
+
+def isTrue(s):
+    return any(s[i] == s[i+1] for i in range(len(s)-1))
 
 def superReducedString(s):
-    while any(s[i] == s[i+1] for i in range(len(s)-1)):
-        for i in range(len(s)-1):
-            if s[i] == s[i+1]:
-                s = s.replace(s[i]+s[i+1], "")
-                break 
-    if len(s) == 0:
-        return 'Empty String'
-    return s
+    if isTrue(s):
+        s = create(s)
+        return superReducedString(s)
+    else:
+        return s if s else 'Empty String'
 
 if __name__ == '__main__':
     s = input()
